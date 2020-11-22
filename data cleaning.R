cb0c00d1af4ede0d67c5c6a80fdcb89dbdd1057d
Zhe Yu <yuzhe998@bu.edu>
2020-11-22 17:51:51 +0800

    update model

--- data cleaning.R
+++ data cleaning.R
@@ -7,6 +7,7 @@ library(magrittr)
 library(kableExtra)
 library(gridExtra)
 library(tidytext)
+library(lubridate)
 data("stop_words")
 
 # import data
@@ -40,45 +41,129 @@ talks %<>% select(-c("de1","de2","de3","de4"))
 ## number of speakers
 ggplot(data = talks) + 
   aes(num_speakers) + 
-  geom_histogram(binwidth = 1,fill="skyblue")
+  geom_histogram(binwidth = 1,fill="skyblue")+
+  labs(title = "distribution of number of speakers",x = "number of speakers")
 
 ##
 ggplot(data = talks) + 
   aes(x=num_lang,y=..density..) + 
-  geom_histogram(binwidth = 2,fill="steelblue")
+  geom_histogram(binwidth = 2,fill="steelblue")+
+  labs(title = "distribution of available languages",x = "number of available languages")
 
 ggplot(data = talks) + 
   aes(comments) + 
   geom_histogram(binwidth = 100,fill="darkseagreen")+
-  xlim(0,2000)
+  xlim(0,2000)+
+  labs(title = "distribution of comments",x = "number of comments")
 
 ggplot(data = talks) + 
   aes(views) + 
-  geom_histogram(binwidth = 500000,fill="aquamarine4")+
-  xlim(0,20000000)
+  geom_histogram(binwidth = 500000,fill="darkorange")+
+  xlim(0,20000000)+
+  labs(title = "distribution of views",x = "number of views")
 
 ggplot(data = talks) + 
-  aes(duration) + 
-  geom_histogram(binwidth = 50,fill="aquamarine4")
+  aes(duration/60) + 
+  geom_histogram(binwidth = 1,fill="hotpink3")+
+  labs(title = "distribution of duration",x = "duration/min")
 
+## occupation
+talks %>%
+count(`first occupation`, sort = TRUE) %>%
+  filter(n > 25) %>%
+  mutate(`first occupation` = reorder(`first occupation`, n)) %>%
+  ggplot(aes(n, `first occupation`)) +
+  geom_col(fill="tan2") +
+  labs(y = NULL)
+  
+## speaker
+talks %>%
+  count(speaker_1, sort = TRUE) %>%
+  filter(n > 4) %>%
+  mutate(speaker_1 = reorder(speaker_1, n)) %>%
+  ggplot(aes(n, speaker_1)) +
+  geom_col(fill="tan3") +
+  labs(y = NULL)
 
+##events
+talks %>%
+  count(event, sort = TRUE) %>%
+  filter(n > 50) %>%
+  mutate(event = reorder(event, n)) %>%
+  ggplot(aes(n, event)) +
+  geom_col(fill="tan4") +
+  labs(y = NULL)
+
+## year
+talks$recorded_date <- ymd(talks$recorded_date)
+talks$published_date <- ymd(talks$published_date)
+talks$published_year <- as.factor(year(talks$published_date))
+talks$published_month <- as.factor(month(talks$published_date))
+ggplot(talks,aes(published_year))+
+  geom_bar()+
+  labs(title = "distribution of published year",x="published year")
+ggplot(talks,aes(published_month))+
+  geom_bar()+
+  labs(title = "distribution of published month",x="published month")
+ggplot(talks,aes(published_date,comments))+geom_line()
+
+## scatter plot
 ggplot(data = talks)+
   aes(views,comments)+
-  geom_point(aes(color=duration))+
-  xlim(0,40000000)
+  geom_point(alpha = 0.3,color = "orchid3")+
+  xlim(0,20000000)+
+  ylim(0,2000)+
+  labs(title = "views vs comments")+
+  geom_smooth(method = "lm")
 
 ggplot(data = talks)+
   aes(duration,comments)+
-  geom_point(aes(color=views))+
-  scale_fill_brewer(direction = -1)
+  geom_point(alpha = 0.3,color = "tomato3")+
+  scale_fill_brewer(direction = -1)+
+  ylim(0,2000)+
+  xlim(0,2000)+
+  labs(title = "comments vs duration")+
+  geom_smooth()
 
 ggplot(data = talks)+
   aes(num_lang,comments)+
-  geom_point(aes(color=duration))
+  geom_point(alpha = 0.3,color = "brown4")+
+  labs(title = "comments vs duration")+
+  geom_smooth()
 
 #text mining
 topics <- data.frame(talks_id = ted_talks_en$talk_id, text = ted_talks_en$topics)
-topics %<>% unnest_tokens(word, text) %>% anti_join(stop_words)%>%count(word,sort = T)
+topics %<>% unnest_tokens(word, text) %>% anti_join(stop_words)
 ggplot(topics[1:10,],aes(word,n))+
   geom_col()+
   coord_flip()
+
+topics %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 500) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word)) +
+  geom_col(fill="tan3") +
+  labs(y = NULL)
+
+##data process
+talks %<>% mutate(duration_min = duration/60)
+talks %<>% mutate(log_views = log(views))
+talks %<>% mutate(log_comments = log(comments))
+talks %<>% filter(log_comments>=0)
+talks %<>% mutate(english = ifelse(native_lang=="en",1,0))
+
+##model
+fit1 <- lm(comments~duration_min+log_views+num_lang,talks)
+summary(fit1)
+
+ggplot()+
+  geom_point(aes(fit1$fitted.values,fit1$residuals))+
+  geom_smooth(aes(fit1$fitted.values,fit1$residuals))
+
+par(mfrow = c(2,2))
+plot(fit1)
+
+fit2 <- lm(log_comments~duration_min+log_views+num_lang,talks)
+par(mfrow = c(2,2))
+plot(fit2)
